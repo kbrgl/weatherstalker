74ff72865c5578bf979ff22cf13a06c2119ce1b3
Kabir Goel <kabirgoel.kg@gmail.com>
2016-03-17 03:08:28 +0530

    Fix missing bracket

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ router.post('/', function *() {
 	} else {
 		let res = yield weather.get(this.request.body.location, config.API_KEY);
 		let weather_object = weather.distill(res);
-		if (this.request.accepts('json', 'html' == 'json')) {
+		if (this.request.accepts('json', 'html') == 'json') {
 			this.body = yield weather_object;
 		} else {
 			this.body = yield render('weather.mustache', weather_object);
