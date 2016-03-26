f45cc13f6b375b7290814a69c6d92c3e55be26e2
Kabir Goel <kabirgoel.kg@gmail.com>
2016-03-26 13:43:03 +0530

    fix config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 /*
  * Acts as a wrapper around config.json
  */
-const config = require('./config.json');
 module.exports = {
     port: process.env.PORT || 4000,
-    API_KEY: process.env.API_KEY || config.API_KEY
+    API_KEY: process.env.API_KEY || require('./config.json').API_KEY
 }
